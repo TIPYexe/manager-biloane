7e631bade178166c4363ae72046a7688bd056f19
Tipy <53595545+TIPYexe@users.noreply.github.com>
2022-06-14 21:05:13 +0300

    add icons on bilon + details + images/issue

--- Bilons/App.js
+++ Bilons/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Modal, TouchableOpacity, FlatList, SafeAreaView, Image, ImageBackground, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Modal, TouchableOpacity, FlatList, SafeAreaView, Image, ImageBackground, Dimensions, ScrollView } from 'react-native';
 // import { TouchableOpacity } from 'react-native-web';
 // import Modal from 'react-native-modal';
 
@@ -20,6 +20,12 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 10,
     borderTopLeftRadius: 10,
   },
+  bilonCount: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    minWidth: 70,
+    textAlign: 'center'
+  },
   modalContainer: {
     flex: 1,
     alignItems: 'flex-start',
@@ -32,12 +38,20 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     flexDirection: 'row'
   },
+  optionContainer: {
+    backgroundColor: 'rgba(0,255,50,0.5)',
+    borderColor: 'black',
+    borderWidth: 3,
+    justifyContent: 'center',
+    borderRadius: 50,
+    paddingHorizontal: 15,
+    height: 45,
+  },
   optionStyle: {
     fontSize: 20,
+    fontWeight: 'bold',
     textAlign: 'center',
     lineHeight: 20,
-    borderRadius: 50,
-    padding: 15
   },
   triangle: {
     width: 0,
@@ -46,11 +60,15 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderLeftWidth: 10,
     borderRightWidth: 10,
-    borderBottomWidth: 20,
+    borderBottomWidth: 10,
     borderLeftColor: 'transparent',
     borderRightColor: 'transparent',
-    borderBottomColor: 'black',
-    marginBottom: -7,
+    marginBottom: -4,
+  },
+  reportShadow: {
+    shadowColor: 'black',
+    shadowOpacity: 1,
+    shadowRadius: 5,
   }
 });
 
@@ -62,7 +80,8 @@ export default function App() {
   const deviceH = Dimensions.get('screen').height;
 
   const ripeImg = ['https://i.ibb.co/72r7VKN/bilon0.png', 'https://i.ibb.co/wLWLrvW/bilon1.png', 'https://i.ibb.co/fkL93Fg/bilon2.png', 'https://i.ibb.co/JCVjcCR/bilon3.png', 'https://i.ibb.co/0GYyhWH/bilon4.png'];
-  const reportTypes = ['sick', 'missing', 'other'];
+  const reportTypes = ['Planta bolnava', 'Planta lipsa', 'Altceva'];
+  const reportImg = ['https://cdn-icons.flaticon.com/png/512/5610/premium/5610989.png?token=exp=1655226659~hmac=381788422552f564452ceda793d0117e', 'https://cdn-icons-png.flaticon.com/512/189/189665.png', 'https://cdn-icons.flaticon.com/png/512/722/premium/722353.png?token=exp=1655226712~hmac=571e72a3cd63ee732852c64ed878b792'];
 
   useEffect(() => {
     if (dataBiloane.length == 0) {
@@ -71,9 +90,9 @@ export default function App() {
         let nr = parseInt(Math.random() * 10);
         for (let j = 1; j <= nr; j++) {
           reportsAux.push({
-            position: `${parseInt(Math.random() * 100)}%`,
-            type: reportTypes[parseInt(Math.random() * 3)],
-            images: []
+            position: parseInt(Math.random() * 20) * 5,
+            type: parseInt(Math.random() * 3),
+            images: ['https://i.ibb.co/QDQc1sV/IMG-20201224-WA0006.jpg', 'https://i.ibb.co/tJ1QvHG/IMG-20201224-WA0022.jpg', 'https://i.ibb.co/h86vKnC/IMG-20201224-WA0027.jpg']
           })
         }
 
@@ -86,11 +105,21 @@ export default function App() {
     }
   }, [dataBiloane]);
 
+  function compare(a, b) {
+    if (a.position < b.position) {
+      return -1;
+    }
+    if (a.position > b.position) {
+      return 1;
+    }
+    return 0;
+  }
+
   const renderBilon = ({ item }) => (
     <TouchableOpacity
       style={styles.bilonContainer}
       onPress={() => { setSelectedBilon(item); setBilonDetails(true); }}>
-      <Text style={{ fontSize: 30, fontWeight: 'bold', minWidth: 70, textAlign: 'center' }}>{item.nr}</Text>
+      <Text style={styles.bilonCount}>{item.nr}</Text>
       <ImageBackground
         resizeMode="contain"
         source={{ uri: ripeImg[item.ripe] }}
@@ -98,7 +127,23 @@ export default function App() {
         imageStyle={[selectedBilon?.nr != item.nr && { flex: 1, height: 60, width: 1920 / 2, left: -10 }]}
       />
     </TouchableOpacity>
-  )
+  );
+
+  const renderReport = ({ item }) => (
+    <View style={{ padding: 10, borderRadius: 15, borderWidth: 1, borderColor: 'rgba(0,0,0,0.3)', marginBottom: 10 }}>
+      <View style={{ flexDirection: 'row' }}>
+        <Image source={{ uri: reportImg[item.type] }} style={{ height: 30, width: 30, marginRight: 10 }} />
+        <Text style={[styles.bilonCount, { fontSize: 20, textAlign: 'left' }]}>{reportTypes[item.type]}</Text>
+      </View>
+      <View style={{ flexDirection: 'row', maxWidth: '100%' }}>
+        <ScrollView horizontal>
+          {item.images.map((prop) => (
+            <Image source={{ uri: prop }} style={{ height: 230, width: 120, borderRadius: 15, margin: 10 }} />
+          ))}
+        </ScrollView>
+      </View>
+    </View >
+  );
 
   return (
     <SafeAreaView>
@@ -114,36 +159,48 @@ export default function App() {
         visible={bilonDetails}
       >
         <View style={styles.modalContainer}>
+
           <View style={styles.modalOptions}>
-            <Text
-              style={[styles.optionStyle, { backgroundColor: 'rgba(0,0,0,0.2)' }]}
-              onPress={() => { setBilonDetails(false); setSelectedBilon(null); }}
-            >X</Text>
-            <Text
-              style={[styles.optionStyle, { backgroundColor: 'rgba(0,255,50,0.5)' }]}
-              onPress={() => { }}
-            >Add</Text>
+            <TouchableOpacity onPress={() => { setBilonDetails(false); setSelectedBilon(null); }}>
+              <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/512/992/992660.png' }} style={{ borderRadius: 50, height: 45, width: 45 }} />
+            </TouchableOpacity>
+            <Text style={styles.bilonCount}>{selectedBilon?.nr}</Text>
+            <TouchableOpacity
+              style={styles.optionContainer}
+              onPress={() => { }}>
+              <Text style={[styles.optionStyle]}>Adauga</Text>
+            </TouchableOpacity>
           </View>
+
           <Image
-            resizeMode="contain"
+            resizeMode="stretch"
             source={{ uri: ripeImg[selectedBilon?.ripe] }}
             style={{
               width: '100%',
-              height: 30,
+              height: 35,
             }} />
-          <View style={{ width: '100%', height: 50 }}>
+
+          <View style={{ width: '100%', height: 50, paddingHorizontal: 5 }}>
             {console.log(selectedBilon?.reports)}
-            {selectedBilon?.reports.map((prop, key) => (
-              <View key={key} style={{ position: 'absolute', left: prop.position, alignItems: 'center', zIndex: key }}>
-                <View style={styles.triangle} />
-                <View style={{ height: 40, width: 40, borderRadius: 50, backgroundColor: 'black' }}>
+            {selectedBilon?.reports.sort(compare).map((prop, key) => (
+              <View key={key} style={[{ position: 'absolute', left: `${prop.position}%`, alignItems: 'center', zIndex: prop.position }]}>
+                <View style={[styles.triangle, { borderBottomColor: 'rgba(0,0,0,1)' }]} />
+                <View style={[{ padding: 2.5, borderRadius: 50, backgroundColor: 'white' }, styles.reportShadow, { elevation: 6 }]}>
+                  <Image source={{ uri: reportImg[prop.type] }} style={{ height: 35, width: 35 }} />
                 </View>
               </View>
-            )
-            )}
+            ))}
           </View>
+
+          <Text style={[styles.bilonCount, { marginVertical: 20 }]}>Probleme raportate</Text>
+
+          <FlatList
+            data={selectedBilon?.reports}
+            renderItem={renderReport}
+          />
+
         </View>
-      </Modal>
+      </Modal >
     </SafeAreaView >
   );
 }
